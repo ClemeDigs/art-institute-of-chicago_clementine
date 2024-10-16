a0ca29f199f8b47ba1bf691af2e5ab53d791d3a0
Clementine Digout <clementine.digout@yahoo.fr>
2024-10-15 20:00:15 -0400

    change comments

--- assets/js/Pager.js
+++ assets/js/Pager.js
@@ -34,83 +34,72 @@ export default class Pager {
     }
 
     /**
-     * @param {number} totalPages - Le nombre total de pages.
+     * @param {number} totalPages
      * @returns {void}
      */
     setTotalPages(totalPages) {
         this.totalPages = totalPages;
-        this.updatePageDisplay(); // Met à jour l'affichage
+        this.updatePageDisplay(); 
     }
 
     /**
-     * Définit la page actuelle et déclenche un rafraîchissement de l'affichage.
      * @param {number} page - La nouvelle page actuelle.
      * @returns {void}
      */
     setCurrentPage(page) {
-        this.currentPage = page; // Met à jour la page actuelle
-        this.updatePageDisplay(); // Rafraîchit l'affichage
+        this.currentPage = page; 
+        this.updatePageDisplay(); 
         this.onPageChange(page); // Appelle la fonction callback avec le nouveau numéro de page
     }
 
     /**
-     * Réinitialise la page actuelle à 1 et rafraîchit l'affichage.
      * @returns {void}
      */
     resetCurrentPage() {
-        this.currentPage = 1; // Réinitialise la page à 1
-        this.updatePageDisplay(); // Met à jour l'affichage
+        this.currentPage = 1; 
+        this.updatePageDisplay();
         this.onPageChange(this.currentPage); // Appelle la fonction callback avec la première page
     }
 
     /**
-     * Met à jour l'affichage de la page actuelle et du nombre total de pages.
      * @returns {void}
      */
     updatePageDisplay() {
         /**
          * @type {HTMLElement}
          */
-        const pageElement = this.pagerElement.querySelector('.info-page'); // Sélecteur pour l'élément affichant la page actuelle
+        const pageElement = this.pagerElement.querySelector('.info-page');
         /**
          * @type {HTMLElement}
          */
-        const totalPagesElement = this.pagerElement.querySelector('.total-pages'); // Sélecteur pour l'élément affichant le nombre total de pages
+        const totalPagesElement = this.pagerElement.querySelector('.total-pages');
 
-        // Vérifier si l'élément de la page actuelle existe avant de modifier son contenu
-        pageElement.textContent = this.currentPage; // Met à jour le texte de l'élément avec le numéro de page actuel
-
-        // Vérifier si l'élément du nombre total de pages existe avant de modifier son contenu
-        totalPagesElement.textContent = `Total pages: ${this.totalPages}`; // Met à jour le texte avec le nombre total de pages
+        pageElement.textContent = this.currentPage; 
+        totalPagesElement.textContent = `Total pages: ${this.totalPages}`; 
     }
 
     /**
-     * Ajoute les listeners sur les boutons "Suivant" et "Précédent".
      * @returns {void}
      */
     addPaginationListeners() {
         /**
          * @type {HTMLElement}
          */
-        const nextButton = this.pagerElement.querySelector('.btn-next'); // Sélectionne le bouton "Suivant"
+        const nextButton = this.pagerElement.querySelector('.btn-next');
         /**
          * @type {HTMLElement}
          */
-        const prevButton = this.pagerElement.querySelector('.btn-previous'); // Sélectionne le bouton "Précédent"
+        const prevButton = this.pagerElement.querySelector('.btn-previous');
 
-        // Ajoute un listener au bouton "Suivant" si l'élément existe
         nextButton.addEventListener('click', () => {
-            // Si la page actuelle est inférieure au total des pages, on passe à la page suivante
             if (this.currentPage < this.totalPages) {
-                this.setCurrentPage(this.currentPage + 1); // Incrémente la page et met à jour l'affichage
+                this.setCurrentPage(this.currentPage + 1);
             }
         });
 
-        // Ajoute un listener au bouton "Précédent" si l'élément existe
         prevButton.addEventListener('click', () => {
-            // Si la page actuelle est supérieure à 1, on passe à la page précédente
             if (this.currentPage > 1) {
-                this.setCurrentPage(this.currentPage - 1); // Décrémente la page et met à jour l'affichage
+                this.setCurrentPage(this.currentPage - 1);
             }
         });
     }
